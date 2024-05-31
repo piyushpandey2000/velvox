205b3056cb971681a9c2a1c13e11204952770657
piyush.pandey <piyush.pandey@clevertap.com>
2024-06-01 05:29:52 +0530

    Initial commit

---
+++ src/main/java/com/velvox/VelvoxApplication.java
@@ -0,0 +1,13 @@
+package com.velvox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelvoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelvoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velvox/VelvoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velvox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelvoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
